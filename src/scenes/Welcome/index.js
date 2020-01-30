8a450c48e136913ef68761ccb0197d548a942996
Yeliana Torres <yeli_andre_@hotmail.com>
2020-01-30 16:41:27 -0500

    hotfiix: handle input error in login

--- src/scenes/Welcome/index.js
+++ src/scenes/Welcome/index.js
@@ -62,7 +62,8 @@ export default class Welcome extends Component {
     }
   }
 
-  handleInputChange(event, { name, value }) {
+  handleInputChange(event) {
+    const { name, value } = event.target;
     let newState = this.state;
     newState.card.fields[name].value = value;
     this.setState(newState);
